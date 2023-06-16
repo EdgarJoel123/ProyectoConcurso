210c711b589f1a709cf8c855443296ed066c5aca
Edgar <esanchez1169@uta.edu.ec>
2023-06-16 11:20:44 -0500

    Base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
+import { VistaMiembrosComponent } from './componentes/vista-miembros/vista-miembros.component';
 
 const routes: Routes = [
   {
     path: '', component: LoginComponent
+  },
+  {
+    path: 'miembros', component: VistaMiembrosComponent
   },
   {
     path: '**', redirectTo:'', pathMatch:'full'
