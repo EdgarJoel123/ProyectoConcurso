84b5c3a8ae5b63f28ba0c01f803ae5ca8da1638d
NANA1207 <74752173+NANA1207@users.noreply.github.com>
2023-06-16 16:11:52 -0500

    proyecto Total

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,7 +1,10 @@
+// LoginComponent.ts
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { Usuario } from 'src/app/models/usuario';
 import { ServiciosService } from '../services/servicios.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,7 +12,6 @@ import { ServiciosService } from '../services/servicios.service';
 })
 export class LoginComponent {
   usuario: Usuario[];
-  
   USUARIO: string;
   CONTRASENA: string;
   loginData: any;
@@ -20,49 +22,47 @@ export class LoginComponent {
     password: ''
   };
 
+  constructor( private service: ServiciosService, private router: Router, private http: HttpClient) {}
 
+  // LoginComponent.ts
+validarUsuario() {
+  const { username, password } = this.formData;
 
-  constructor(private service: ServiciosService, private router: Router, private http: HttpClient) {
+  this.service.login(username, password).subscribe(
+    users => {
+      const user = users.find(u => u.CEDULAPERSONAL === username && u.CONTRASENAPERSONAL === password);
 
-   }
-  
- 
-
-
-  validarUsuario() {
-    const { username, password } = this.formData;
-
-    this.service.login(username, password).subscribe(
-      users => {
-        const user = users.find(u => u.USUARIO === username && u.CONTRASENA === password);
-
-        if (user) {
-          if (user.ROL === 'ASESOR') {
-            console.log('Inicio de sesión exitoso como Asesor');
-            alert('Inicio de sesión exitoso como Asesor' );
-            
-            this.router.navigate(['/vista-asesor']);
-          } else if (user.ROL === 'ADMIN') {
-            console.log('Inicio de sesión exitoso como Admin')
-            alert('Inicio de sesión exitoso como Admin');
-            
-            this.router.navigate(['/vista-admin']);
-          }
-          
-          this.service.setOdservable=user.id;
-        } else {
-          console.log('Usuario o contraseña incorrectos');
-          alert('Usuario o contraseña incorrectos');
-            // Limpiar campos de usuario y contraseña
-            this.formData.username= '';
-            this.formData.password = '';
+      if (user) {
+        if (user.ROLPERSONAL === 'ADMINISTRADOR') {
+          console.log('Inicio de sesión exitoso como ADMINISTRADOR');
+          alert('Inicio de sesión exitoso como ADMINISTRADOR');
+          this.service.setLoginData({ username: user.CEDULAPERSONAL }); // Guardar los datos del inicio de sesión en el AuthService
+          this.router.navigate(['/admin']);
+        } else if (user.ROLPERSONAL === 'MIEMBRO') {
+          console.log('Inicio de sesión exitoso como MIEMBRO');
+          alert('Inicio de sesión exitoso como MIEMBRO');
+          this.service.setLoginData({ username: user.CEDULAPERSONAL }); // Guardar los datos del inicio de sesión en el AuthService
+          this.router.navigate(['/miembros']);
+        } else if (user.ROLPERSONAL === 'GERENTE') {
+          console.log('Inicio de sesión exitoso como GERENTE');
+          alert('Inicio de sesión exitoso como GERENTE');
+          this.service.setLoginData({ username: user.CEDULAPERSONAL }); // Guardar los datos del inicio de sesión en el AuthService
+          this.router.navigate(['/generente']);
         }
-      },
-      error => {
-        console.error('Error en la autenticación', error);
-        this.formData.username= '';
+      } else {
+        console.log('Usuario o contraseña incorrectos');
+        alert('Usuario o contraseña incorrectos');
+        // Limpiar campos de usuario y contraseña
+        this.formData.username = '';
         this.formData.password = '';
       }
-    );
-  }
+    },
+    error => {
+      console.error('Error en la autenticación', error);
+      this.formData.username = '';
+      this.formData.password = '';
+    }
+  );
+}
+
 }
