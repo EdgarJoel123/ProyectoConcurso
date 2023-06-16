8458a44db5798c045155b23784b44e2d8231b6a3
NANA1207 <74752173+NANA1207@users.noreply.github.com>
2023-06-16 13:27:10 -0500

    nuevos cambios

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,9 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Usuario } from 'src/app/models/usuario';
 import { ServiciosService } from '../services/servicios.service';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,7 +12,7 @@ export class LoginComponent {
 
 
   usuario: Usuario[];
-  
+
   USUARIO: string;
   CONTRASENA: string;
 
@@ -28,8 +27,8 @@ export class LoginComponent {
   constructor(private service: ServiciosService, private router: Router, private http: HttpClient) {
 
    }
-  
- 
+
+
 
 
   validarUsuario() {
@@ -37,22 +36,27 @@ export class LoginComponent {
 
     this.service.login(username, password).subscribe(
       users => {
-        const user = users.find(u => u.USUARIO === username && u.CONTRASENA === password);
+        const user = users.find(u => u.CEDULAPERSONAL === username && u.CONTRASENAPERSONAL === password);
 
         if (user) {
-          if (user.ROL === 'ASESOR') {
-            console.log('Inicio de sesión exitoso como Asesor');
-            alert('Inicio de sesión exitoso como Asesor' );
-            
-            this.router.navigate(['/vista-asesor']);
-          } else if (user.ROL === 'ADMIN') {
-            console.log('Inicio de sesión exitoso como Admin')
-            alert('Inicio de sesión exitoso como Admin');
-            
-            this.router.navigate(['/vista-admin']);
+          if (user.ROLPERSONAL === 'ADMINISTRADOR') {
+            console.log('Inicio de sesión exitoso como ADMINISTRADOR');
+            alert('Inicio de sesión exitoso como ADMINISTRADOR' );
+
+            this.router.navigate(['/admin']);
+          } else if (user.ROLPERSONAL === 'MIEMBRO') {
+            console.log('Inicio de sesión exitoso como MIEMBRO')
+            alert('Inicio de sesión exitoso como MIEMBRO');
+
+            this.router.navigate(['/miembros']);
+          } else if (user.ROLPERSONAL === 'GERENTE') {
+            console.log('Inicio de sesión exitoso como GERENTE')
+            alert('Inicio de sesión exitoso como GERENTE');
+            this.router.navigate(['/generente']);
+
           }
-          
-          this.service.setOdservable=user.id;
+
+          this.service.setOdservable=user.ID_PERSONAL;
         } else {
           console.log('Usuario o contraseña incorrectos');
           alert('Usuario o contraseña incorrectos');
